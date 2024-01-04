43b471169d9440f56d0672cfb70dee38b3f98b8c
rodrigoctoledo <rodrigo_toledo@outlook.com>
2024-01-04 17:07:20 -0300

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.lblFahrenheit = new System.Windows.Forms.TextBox();
+            this.lblKelvin = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(225, 166);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(75, 23);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "btnConverter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(88, 166);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 22);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.Location = new System.Drawing.Point(88, 218);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(362, 22);
+            this.lblFahrenheit.TabIndex = 2;
+            // 
+            // lblKelvin
+            // 
+            this.lblKelvin.Location = new System.Drawing.Point(88, 264);
+            this.lblKelvin.Name = "lblKelvin";
+            this.lblKelvin.Size = new System.Drawing.Size(362, 22);
+            this.lblKelvin.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKelvin);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.btnConverter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConverter;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.TextBox lblFahrenheit;
+        private System.Windows.Forms.TextBox lblKelvin;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtCelsius.Text, out double temperaturaCelsius))
+            {
+                // Converter para Kelvin
+                double temperaturaKelvin = CelsiusParaKelvin(temperaturaCelsius);
+
+                // Converter para Fahrenheit
+                double temperaturaFahrenheit = CelsiusParaFahrenheit(temperaturaCelsius);
+
+                // Exibir resultados
+                lblKelvin.Text = $"Temperatura em Kelvin: {temperaturaKelvin} K";
+                lblFahrenheit.Text = $"Temperatura em Fahrenheit: {temperaturaFahrenheit} °F";
+            }
+            else
+            {
+                MessageBox.Show("Entrada inválida. Certifique-se de inserir um número válido para a temperatura em Celsius.");
+            }
+        }
+
+        private double CelsiusParaKelvin(double temperaturaCelsius)
+        {
+            // KELVIN = Tc + 273.15
+            return temperaturaCelsius + 273.15;
+        }
+
+        private double CelsiusParaFahrenheit(double temperaturaCelsius)
+        {
+            // FAHRENHEIT = (Tf - 32) * 5 / 9
+            return (temperaturaCelsius * 9 / 5) + 32;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
